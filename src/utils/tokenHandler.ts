34dc8345ac658a3a6028a60fa424898bf5bd7127
valevhf <vale.belen.vhf@gmail.com>
2024-04-29 20:00:50 -0400

    Add tokenHandler

---
+++ src/utils/tokenHandler.ts
@@ -0,0 +1,30 @@
+'use server'
+import { cookies } from 'next/headers'
+
+const tokenHandler = {
+  saveToken: (token: string) => {
+    cookies().set('token', token, {
+      expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //cookie expires in 3 hours
+      path: '/',
+      secure: true,
+      httpOnly: true,
+    })
+  },
+
+  checkToken: () => {
+    const hasToken = cookies().has('token')
+    return hasToken
+  },
+
+  retrieveToken: () => {
+    const token = cookies().get('token')
+    return token
+  },
+
+  removeToken: () => {
+    cookies().delete('token')
+  },
+}
+
+export const { saveToken, retrieveToken, removeToken, checkToken } =
+  tokenHandler
